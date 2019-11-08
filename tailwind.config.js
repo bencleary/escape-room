37ba314b2a5cbc026d2b0a37bcf6189552e79bd9
Ben Cleary <benjamin.joel.cleary@gmail.com>
2019-11-08 21:29:36 +0000

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'reaper-red': '#d63031'
+      },
+      width: {
+        'w-320': '320px'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
