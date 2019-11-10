7e612070baddccbe6ffa6775ec515c46d95fa564
Ben Cleary <benjamin.joel.cleary@gmail.com>
2019-11-10 14:21:14 +0000

    added routing pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   theme: {
+    screens: {
+      'xs': '320px',
+      'sm': '480px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {
       colors: {
         'reaper-red': '#d63031'
